907fb25327bf4e0c888de6e3abfb55ef8745cd6c
liangzhiyuan01 <liangzhiyuan01@baidu.com>
2018-02-20 21:02:12 +0800

    add missed files

---
+++ NaiveC/NaiveC.h
@@ -0,0 +1,14 @@
+//
+//  NaiveC.h
+//  NaiveC
+//
+//  Created by Liang,Zhiyuan(GIS) on 2018/2/20.
+//  Copyright © 2018年 Ogreaxe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double NaiveCVersionNumber;
+FOUNDATION_EXPORT const unsigned char NaiveCVersionString[];
+
+#import <NaiveC/NCCodeEngine_iOS.h>
