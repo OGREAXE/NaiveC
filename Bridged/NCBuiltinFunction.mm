5f0ea3fbe35170ed7105e166111dec6060baf86b
liangzhiyuan01 <liangzhiyuan01@baidu.com>
2018-04-08 10:11:23 +0800

    add getObject

--- Bridged/NCBuiltinFunction.mm
+++ Bridged/NCBuiltinFunction.mm
@@ -9,6 +9,9 @@
 #include "NCBuiltinFunction.hpp"
 #import <UIKit/UIKit.h>
 
+#include <cstdlib>
+#include "NCCocoaBox.hpp"
+
 NCBuiltinPrint::NCBuiltinPrint(){
     name = "print";
     parameters.push_back(shared_ptr<NCParameter>(new NCParameter("string","str")));
@@ -24,4 +27,32 @@ bool NCBuiltinPrint::invoke(vector<shared_ptr<NCStackElement>> &arguments){
 
 bool NCBuiltinPrint::invoke(vector<shared_ptr<NCStackElement>> &arguments,vector<shared_ptr<NCStackElement>> & lastStack){
     return invoke(arguments);
+}
+
+NCBuiltinGetObject::NCBuiltinGetObject(){
+    name = "getObject";
+    parameters.push_back(shared_ptr<NCParameter>(new NCParameter("string","str")));
+}
+
+bool NCBuiltinGetObject::invoke(vector<shared_ptr<NCStackElement>> &arguments){
+    
+    return true;
+}
+
+bool NCBuiltinGetObject::invoke(vector<shared_ptr<NCStackElement>> &arguments,vector<shared_ptr<NCStackElement>> & lastStack){
+    auto str = arguments[0]->toString();
+//    NSString * nestedString = [NSString stringWithUTF8String:str.c_str()];
+    char * end;
+    unsigned long long ullAddr = strtoull(str.c_str(),&end, 16);
+    
+    if (errno == ERANGE) {
+        NSLog(@"error range");
+        return false;
+    }
+    
+    NCCocoaBox * cbox = new NCCocoaBox((void*)ullAddr);
+    
+    lastStack.push_back(shared_ptr<NCStackPointerElement>(new NCStackPointerElement(shared_ptr<NCObject>(cbox))));
+    
+    return true;
 }
